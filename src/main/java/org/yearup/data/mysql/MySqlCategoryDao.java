d06d8bb871cfb4ce41274040660bafb3e7832276
wasidc <wasid.chowdhury@icloud.com>
2025-12-19 08:48:48 -0500

    create a new category,using try and catch

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -19,26 +19,25 @@ import java.sql.Connection;
 @Component
 public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
-    public MySqlCategoryDao(DataSource ds)
-    {
+    public MySqlCategoryDao(DataSource ds) {
 
         super(ds);
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
+    public List<Category> getAllCategories() {
         List<Category> categories = new ArrayList<>();
 
-        try(
+        try (
                 Connection c = ds.getConnection();
                 PreparedStatement q = c.prepareStatement("""
                         SELECT category_id, name, description
                         FROM categories
                         """);
                 ResultSet r = q.executeQuery();
-        ){
-            while(r.next()){
+
+        ) {
+            while (r.next()) {
                 Category cat = new Category();
                 cat.setCategoryId(r.getInt("Category_id"));
                 cat.setName(r.getString("Name"));
@@ -46,55 +45,76 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
                 categories.add(cat);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error getting all categories");
         }
 
         return categories;
     }
 
-
-
     @Override
-    public Category getById(int categoryId)
+    public Category getById(int categoryId) {
     {
         // get category by id
         return null;
     }
 
-    @Override
-    public Category create(Category category)
-    {
-        // create a new category
-        return null;
+        public Category create(Category category) {
+            Category cat = new Category();
+
+            try(Connection c = ds.getConnection();
+                PreparedStatement q = c.prepareStatement("""
+                SELECT 
+                    category_id, name, description
+                FROM 
+                    categories
+                WHERE 
+                    category_id = ?
+                """)){
+                q.setInt(1,category.getCategoryId());
+
+                ResultSet r = q.executeQuery();
+
+                if(r.next()){
+                    cat.setCategoryId(r.getInt("Category_id"));
+                    cat.setName(r.getString("Name"));
+                    cat.setDescription(r.getString("Description"));
+                }else{
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+                }
+            }catch(SQLException e){
+                System.out.println("Error getting category with id: " + category.getCategoryId());
+            }
+            return cat;
+
+        }
+
+        @Override
+        public void update(int categoryId, Category category)
+        {
+            // update category
+        }
+
+        @Override
+        public void delete(int categoryId)
+        {
+            // delete category
+        }
+
+        private Category mapRow(ResultSet row) throws SQLException
+        {
+            int categoryId = row.getInt("category_id");
+            String name = row.getString("name");
+            String description = row.getString("description");
+
+            Category category = new Category()
+            {{
+                setCategoryId(categoryId);
+                setName(name);
+                setDescription(description);
+            }};
+
+            return category;
+        }
+
     }
-
-    @Override
-    public void update(int categoryId, Category category)
-    {
-        // update category
-    }
-
-    @Override
-    public void delete(int categoryId)
-    {
-        // delete category
-    }
-
-    private Category mapRow(ResultSet row) throws SQLException
-    {
-        int categoryId = row.getInt("category_id");
-        String name = row.getString("name");
-        String description = row.getString("description");
-
-        Category category = new Category()
-        {{
-            setCategoryId(categoryId);
-            setName(name);
-            setDescription(description);
-        }};
-
-        return category;
-    }
-
-}
